c0300c1328085b1d7e7fd7a6c742f4101381833d
Infinitiv525 <104232927+Infinitiv525@users.noreply.github.com>
2024-01-26 15:02:37 +0100

    pridanie buttonikov

--- flask_app.py
+++ flask_app.py
@@ -1759,4 +1759,4 @@ var = []
 lang = importlib.import_module('langs.slovak')
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8000, debug=True)
+    app.run(host='127.0.0.1', port=8000,debug=True)
